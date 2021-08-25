a836f3327a4d500a8b089ae36c1f9cfbb16d019f
LehaGer <leha.ger97@mail.ru>
2021-08-25 15:18:10 +0300

    feat: created component MyButton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
   return (
     <div className="App">
-
+      <MyButton disabled>Create new post</MyButton>
     </div>
   );
 }
